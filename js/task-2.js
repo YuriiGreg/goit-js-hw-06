99562bd3e74d654e7a7e86aa275dda5375125f5b
YuriiGreg <yuriyf888@gmail.com>
2024-04-15 15:26:57 +0200

    js-hw-06

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,30 @@
-function getShippingMessage(country, price, deliveryFee) {
-    const totalPrice = price + deliveryFee;
-    return `Shipping to ${country} will cost ${totalPrice} credits`;
+class Storage {
+    constructor(inicialItems) {
+        this.items = inicialItems;
+    }
+    
+    getItems() {
+        return this.items;
+    }
+    addItem(newItem) {
+        this.items.push(newItem);
+    }
+    removeItem(itemToRemove) {
+         const index = this.items.indexOf(itemToRemove);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+    }
 }
 
-getShippingMessage();
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-console.log(getShippingMessage("Australia", 120, 50));
-console.log(getShippingMessage("Germany", 80, 20));
-console.log(getShippingMessage("Sweden", 100, 20)); 
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
